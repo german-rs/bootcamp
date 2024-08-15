f1f1489352c9124c04fc933901ab754ebc7a9632
german-rs <ing.germanriveros@gmail.com>
2024-08-14 21:54:44 -0400

    Funciones con el DOM

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+document.getElementById("textoSaludo").innerHTML = "Este párrafo fue modificado";
+
+document.getElementById("entradaUno").value = "Click!"; 
+
+document.getElementById("entradaUno").type = "button"; 
+
+document.getElementById("entradaUno").setAttribute("style","color:red; background-color:yellow"); 
+
